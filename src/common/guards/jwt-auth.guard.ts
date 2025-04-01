644dca94fe669bbc80228bba6ce03e3bf0a82188
PhongNDH <phongndhdev@gmail.com>
2025-04-01 23:33:34 +0700

    first commit

--- src/common/guards/jwt-auth.guard.ts
+++ src/common/guards/jwt-auth.guard.ts
@@ -1,21 +1,26 @@
 import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from "@nestjs/common";
 import { JwtService } from "@nestjs/jwt";
+import { ConfigService } from "@nestjs/config";
 
 @Injectable()
 export class JwtAuthGuard implements CanActivate {
-  constructor(private readonly jwtService: JwtService) {
+  constructor(private readonly jwtService: JwtService, private readonly configService : ConfigService) {
   }
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const request = context.switchToHttp().getRequest();
-    const token = request.headers.authorization?.split(" ")[1];
-
+    const token = request?.signedCookies?.['auth_token'];
     if (!token) {
       throw new UnauthorizedException();
     }
-
     try {
-      request.user = await this.jwtService.verifyAsync(token);
+      request.user = await this.jwtService.verifyAsync(
+        token,
+        {
+          secret: this.configService.getOrThrow("JWT_SECRET")
+        }
+      );
+
       return true;
     } catch (error) {
       if (error.name === "TokenExpiredError") {
